cdecb34e4b1b0cdec21014003763906bbda01eee
wangzongwei <32136263+CoderWZW@users.noreply.github.com>
2023-03-31 10:40:13 +0800

    Update recommend_parser.py

--- conf/recommend_parser.py
+++ conf/recommend_parser.py
@@ -13,7 +13,7 @@ def recommend_parse_args():
 
     # ===== model ===== #
     parser.add_argument('--model_name', type=str, default='GMF', help='[GMF,WRMF,LightGCN,NCF,NGCF,SGL,SimGCL]')
-    parser.add_argument('--maxEpoch', type=int, default=10, help='number of epochs')
+    parser.add_argument('--maxEpoch', type=int, default=50, help='number of epochs')
     parser.add_argument('--batch_size', type=int, default=256, help='batch size')
     parser.add_argument('--emb_size', type=int, default=32, help='embedding size')
     parser.add_argument('--n_layers', type=int, default=2, help='number of gnn layers')
